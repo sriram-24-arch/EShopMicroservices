2bbf6092b533dae673f5fc2d3581891365470896
sriramsekar241995 <sriramsekar241995@outlook.com>
2025-10-28 12:20:22 +0530

    Commit Catalog API

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add Services to the Container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
+var app = builder.Build();
+
+app.MapCarter();
+
+// Configure the HTTP request pipeline.
+app.Run();
